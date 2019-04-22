149fc6e81ef1b045f8950cdea49ca17863e5b8f8
anugotta <anugotta@gmail.com>
2019-04-22 15:03:41 +0530

    Bug fixes and Cleanup

--- app/src/main/java/com/asp/fliptimer/MainActivity.kt
+++ app/src/main/java/com/asp/fliptimer/MainActivity.kt
@@ -2,6 +2,8 @@ package com.asp.fliptimer
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.Toast
+import com.asp.fliptimerviewlibrary.CountDownClock
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : Activity() {
@@ -10,5 +12,16 @@ class MainActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         timerProgramCountdown.startCountDown(99999999)
+        timerProgramCountdown.setCountdownListener(object : CountDownClock.CountdownCallBack {
+            override fun countdownAboutToFinish() {
+                //TODO Add your code here
+            }
+
+            override fun countdownFinished() {
+                Toast.makeText(this@MainActivity, "Finished", Toast.LENGTH_SHORT).show()
+                timerProgramCountdown.resetCountdownTimer()
+            }
+        })
+
     }
 }
