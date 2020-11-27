2e5a4d8d5a5678481d438e304d03c6ee8379f2e2
Sven Bendel <ubuntudroid@gmail.com>
2020-11-27 14:33:01 +0100

    Migrate to androidx

--- app/src/main/java/com/asp/fliptimer/MainActivity.kt
+++ app/src/main/java/com/asp/fliptimer/MainActivity.kt
@@ -2,7 +2,7 @@ package com.asp.fliptimer
 
 import android.app.Activity
 import android.os.Bundle
-import android.support.v4.content.res.ResourcesCompat
+import androidx.core.content.res.ResourcesCompat
 import android.widget.Toast
 import com.asp.fliptimerviewlibrary.CountDownClock
 import kotlinx.android.synthetic.main.activity_main.*
